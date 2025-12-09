7a77962774e4063b809cd1528f19e2b94f83e5f4
akshitbeniwal <akshitbeniwal26@gmail.com>
2025-12-09 02:03:23 +0000

    updated the xnat-jupyter plugin to 1.3.3

--- 0-xnat-jupyter-plugin.sh
+++ 0-xnat-jupyter-plugin.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-# Install XNAT JupyterHub Plugin
 set -e
 
 echo "=========================================="
-echo "Installing XNAT JupyterHub Plugin"
+echo "Installing XNAT JupyterHub Plugin v1.3.3"
 echo "=========================================="
 
-# Get XNAT plugin JAR (check latest version number)
+# Get XNAT plugin JAR (v1.3.3)
 echo "[1/4] Downloading plugin..."
-wget https://github.com/NrgXnat/xnat-jupyterhub-plugin/releases/download/v1.1.1/xnat-jupyterhub-plugin-1.1.1.jar
+wget https://github.com/NrgXnat/xnat-jupyterhub-plugin/releases/download/v1.3.3/xnat-jupyterhub-plugin-1.3.3.jar
 
 # Get NFS server pod name dynamically
 echo "[2/4] Finding NFS server pod..."
-NFS_POD=$(kubectl -n storage get pods -l app=nfs-server -o jsonpath='{.items[0].metadata.name}')
+NFS_POD=$(microk8s kubectl -n storage get pods -l role=nfs-server -o jsonpath='{.items[0].metadata.name}')
 echo "Found NFS pod: $NFS_POD"
 
 # Copy to XNAT plugins directory
 echo "[3/4] Copying plugin to NFS..."
-kubectl -n storage cp xnat-jupyterhub-plugin-1.1.1.jar \
+microk8s kubectl -n storage cp xnat-jupyterhub-plugin-1.3.3.jar \
   $NFS_POD:/exports/xnat/plugins/
 
 # Restart XNAT to load plugin
 # !!!!Beware this will wipe any ephemeral data in XNAT pods
 echo "[4/4] Restarting XNAT..."
-kubectl -n ais-xnat rollout restart statefulset/xnat-web
+microk8s kubectl -n ais-xnat rollout restart statefulset/xnat-web
 
 # Wait for XNAT to be ready
-kubectl -n ais-xnat rollout status statefulset/xnat-web
+microk8s kubectl -n ais-xnat rollout status statefulset/xnat-web
 
 echo ""
 echo "✓ Plugin installed successfully"
